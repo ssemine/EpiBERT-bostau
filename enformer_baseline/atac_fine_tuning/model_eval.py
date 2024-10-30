ead0f59ca319c8bcacda78c9199ebb49f40c3d46
njaved <njaved@mit.edu>
2024-10-30 12:15:39 -0400

    updatel oading code

--- enformer_baseline/atac_fine_tuning/model_eval.py
+++ enformer_baseline/atac_fine_tuning/model_eval.py
@@ -158,9 +158,8 @@ def main():
             options = tf.train.CheckpointOptions(experimental_io_device="/job:localhost")
             checkpoint = tf.train.Checkpoint(module=enformer_model)
             tf.saved_model.LoadOptions(experimental_io_device='/job:localhost')
-            #latest = tf.train.checkpoint(wandb.config.checkpoint_path)
-            status = checkpoint.restore(wandb.config.checkpoint_path,options=options)
-            status.assert_existing_objects_matched()
+            latest = tf.train.checkpoint(wandb.config.checkpoint_path)
+            checkpoint.restore(latest,options=options).assert_existing_objects_matched()
 
             print('computing quant metrics')
             pred_list = [] # list to store predictions
