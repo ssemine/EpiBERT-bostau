64206f03ac142ac7549c51b56f62daf4cd56e3be
ssemine <s.semine@uqconnect.edu.au>
2025-05-23 07:47:09 +1000

    fix

--- utils/50k_peaks.sh
+++ utils/50k_peaks.sh
@@ -6,7 +6,7 @@
 #SBATCH --error=logs/atac_paired_%A_%a.err
 #SBATCH --array=0-207
 #SBATCH --cpus-per-task=8
-#SBATCH --mem=1000G
+#SBATCH --mem=64G
 #SBATCH --time=08:00:00
 #SBATCH --partition=general
 
@@ -17,10 +17,7 @@ module load bedtools
 
 mkdir -p logs
 
-
-
 source ../config.sh
-source activate atac
 
 GENOME_DIR="$SCRATCH_DIR/EpiBERT-bostau_data/refGenomes/bovine"
 ATAC_DIR="$SCRATCH_DIR/EpiBERT-bostau_data/bovineATAC/arrayExpress"
@@ -30,13 +27,13 @@ GENOME_SIZE="2.7e9"
 INPUT_DIR="$ATAC_DIR"
 OUTPUT_DIR="$ATAC_DIR/output"
 
-alias trim_galore="./~/.local/bin/TrimGalore-0.6.10/trim_galore"
+TRIM_GALORE="/home/s4693165/.local/bin/TrimGalore-0.6.10/trim_galore"
 
 mkdir -p "$OUTPUT_DIR"
 
-
-R1_FILES=(${INPUT_DIR}/*_R1.fastq.gz)
-R2_FILES=(${INPUT_DIR}/*_R2.fastq.gz)
+R1_FILES=($(find "$INPUT_DIR" -name "*_R1.fastq.gz" | sort))
+R2_FILES=($(find "$INPUT_DIR" -name "*_R2.fastq.gz" | sort))R1_FILES=($(find "$INPUT_DIR" -name "*_R1.fastq.gz" | sort))
+R2_FILES=($(find "$INPUT_DIR" -name "*_R2.fastq.gz" | sort))
 R1="${R1_FILES[$SLURM_ARRAY_TASK_ID]}"
 R2="${R2_FILES[$SLURM_ARRAY_TASK_ID]}"
 sample=$(basename "$R1" _R1.fastq.gz)
@@ -44,13 +41,14 @@ sample=$(basename "$R1" _R1.fastq.gz)
 echo "Processing $sample..."
 
 # trimming
-trim_galore --paired --atac -o "$OUTPUT_DIR" "$R1" "$R2"
+
+"$TRIM_GALORE" --paired -o "$OUTPUT_DIR" "$R1" "$R2"
 
 R1_TRIM="$OUTPUT_DIR/${sample}_R1_val_1.fq.gz"
 R2_TRIM="$OUTPUT_DIR/${sample}_R2_val_2.fq.gz"
 
 # align
-bowtie2 -p 8 -x "$BOWTIE2_IDX" -1 "$R1_TRIM" -2 "$R2_TRIM" -S "$OUTPUT_DIR/$sample.sam" \
+bowtie2 -x "$BOWTIE2_IDX" -1 "$R1_TRIM" -2 "$R2_TRIM" -S "$OUTPUT_DIR/$sample.sam" -p 8 \
     2> "$OUTPUT_DIR/$sample.bowtie2.log"
 
 # sam bam
