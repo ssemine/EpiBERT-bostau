b62dfdb9a3a227a5924d085e5278b77ab05fda2f
Stephen Semine <s4693165@uqconnect.edu.au>
2025-05-23 07:08:40 +1000

    Added peak calling script

---
+++ utils/50k_peaks.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+#SBATCH --account=
+#SBATCH --job-name=atac_paired
+#SBATCH --output=logs/atac_paired_%A_%a.out
+#SBATCH --error=logs/atac_paired_%A_%a.err
+#SBATCH --array=0-207
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=1000G
+#SBATCH --time=08:00:00
+#SBATCH --partition=general
+
+module load bowtie2
+module load samtools
+module load macs2
+module load bedtools
+
+mkdir -p logs
+
+
+
+source ../config.sh
+GENOME_DIR="$SCRATCH_DIR/EpiBERT-bostau_data/refGenomes/bovine"
+ATAC_DIR="$SCRATCH_DIR/EpiBERT-bostau_data/bovineATAC/arrayExpress"
+REF_FA="$GENOME_DIR/bosTau9.fa"
+BOWTIE2_IDX="$GENOME_DIR/bosTau9"
+GENOME_SIZE="2.7e9"
+INPUT_DIR="$ATAC_DIR"
+OUTPUT_DIR="$ATAC_DIR/output"
+
+alias trim_galore="./~/.local/bin/TrimGalore-0.6.10/trim_galore"
+
+mkdir -p "$OUTPUT_DIR"
+
+
+R1_FILES=(${INPUT_DIR}/*_R1.fastq.gz)
+R2_FILES=(${INPUT_DIR}/*_R2.fastq.gz)
+R1="${R1_FILES[$SLURM_ARRAY_TASK_ID]}"
+R2="${R2_FILES[$SLURM_ARRAY_TASK_ID]}"
+sample=$(basename "$R1" _R1.fastq.gz)
+
+echo "Processing $sample..."
+
+# trimming
+trim_galore --paired --atac -o "$OUTPUT_DIR" "$R1" "$R2"
+
+R1_TRIM="$OUTPUT_DIR/${sample}_R1_val_1.fq.gz"
+R2_TRIM="$OUTPUT_DIR/${sample}_R2_val_2.fq.gz"
+
+# align
+bowtie2 -p 8 -x "$BOWTIE2_IDX" -1 "$R1_TRIM" -2 "$R2_TRIM" -S "$OUTPUT_DIR/$sample.sam" \
+    2> "$OUTPUT_DIR/$sample.bowtie2.log"
+
+# sam bam
+samtools view -@ 8 -bS "$OUTPUT_DIR/$sample.sam" | \
+    samtools sort -@ 8 -o "$OUTPUT_DIR/$sample.sorted.bam"
+rm "$OUTPUT_DIR/$sample.sam"
+
+samtools fixmate -m "$OUTPUT_DIR/$sample.sorted.bam" "$OUTPUT_DIR/$sample.fixmate.bam"
+samtools sort -@ 8 -o "$OUTPUT_DIR/$sample.positionsort.bam" "$OUTPUT_DIR/$sample.fixmate.bam"
+samtools markdup -r "$OUTPUT_DIR/$sample.positionsort.bam" "$OUTPUT_DIR/$sample.dedup.bam"
+samtools index "$OUTPUT_DIR/$sample.dedup.bam"
+rm "$OUTPUT_DIR/$sample.fixmate.bam" "$OUTPUT_DIR/$sample.positionsort.bam" "$OUTPUT_DIR/$sample.sorted.bam"
+
+# peak call
+macs2 callpeak -t "$OUTPUT_DIR/$sample.dedup.bam" -f BAMPE -g "$GENOME_SIZE" -n "$sample" \
+    --outdir "$OUTPUT_DIR" --keep-dup all -B --SPMR
+
+# top 50k
+zcat "$OUTPUT_DIR/${sample}_peaks.narrowPeak.gz" | \
+    sort -k5,5nr | head -n 50000 | \
+    awk 'OFS="\t" {center = int(($2 + $3)/2); print $1, center - 64, center + 64}' | \
+    sort -k1,1 -k2,2n > "$OUTPUT_DIR/$sample.top50000.centered.bed"
+
+# get fasta
+bedtools getfasta -fi "$REF_FA" -bed "$OUTPUT_DIR/$sample.top50000.centered.bed" \
+    -fo "$OUTPUT_DIR/$sample.top50000.peaks.fasta"
+
+echo "Done $sample"
+
