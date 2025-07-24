64b8a24ae3382e7868144db1cbaf007257fcbfff
Stephen Semine <stephensemine@mac-ss.local>
2025-07-25 07:51:53 +1000

    fix

--- utils/generate_bg.sh
+++ utils/generate_bg.sh
@@ -3,13 +3,11 @@
 source ../config.sh
 cd "$DATA_PROCESSING_DIR"
 
-for bfile in "$OUTPUT_DIR/unfiltered/"*.top50000.centered.bed; do
-    sample=$(basename "$bfile" .top50000.centered.bed)
+for bfile in "$FILTERED_BED_DIR"*.top50000.centered.filtered.bed; do
+    sample=$(basename "$bfile" .top50000.centered.filtered.bed)
 
     echo "Processing sample: $sample"
-    
-    echo "Filtering union peaks for $sample..."
-    bedtools subtract -a union_peaks.filtered.bed -b "$bfile" > "$FILTERED_BED_DIR/${sample}_bg.bed"
+    bedtools subtract -a union_peaks.bed -b "$bfile" > "$FILTERED_BED_DIR/${sample}_bg.bed"
     echo "Filtering union peaks for $sample..."
     bedtools getfasta -fi "$REF_FA" -bed "$FILTERED_BED_DIR/${sample}_bg.bed" -fo "$FILTERED_FASTA_DIR/${sample}_bg.fasta"
 done
