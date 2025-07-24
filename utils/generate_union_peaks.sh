18b9a0d58c5c3e82599ca5f998424efcb8204130
Stephen Semine <stephensemine@mac-ss.local>
2025-07-25 07:26:17 +1000

    a

--- utils/generate_union_peaks.sh
+++ utils/generate_union_peaks.sh
@@ -2,8 +2,10 @@
 
 source ../config.sh
 cd "$DATA_PROCESSING_DIR"
+module load bedtools
 
 cat $UNFILTERED_DIR/*.bed > all_peaks.bed
-sort -k1,1 -k2,2n all_peaks.bed > all_peaks.sorted.bed
+awk '$1 ~ /^chr([1-9]|1[0-9]|2[0-9]|X|Y)$/' all_peaks.bed > all_peaks.filtered.bed
+sort -k1,1 -k2,2n all_peaks.filtered.bed > all_peaks.sorted.bed
 bedtools merge -i all_peaks.sorted.bed > union_peaks.bed
 
